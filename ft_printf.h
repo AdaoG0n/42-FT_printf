9dda20a972146dde576cf6a8f3a6f1cc54cf6ba8
Adao Manuel Marques Goncalves <adamarqu@c1r10s5.42porto.com>
2024-10-31 13:38:10 +0000

    added

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adamarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 11:14:41 by adamarqu          #+#    #+#             */
+/*   Updated: 2024/10/29 11:14:47 by adamarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+# include <limits.h>
+# include <stdio.h>
+
+int		ft_printf(const char *__format, ...);
+int		prt_str(char const *str);
+int		prt_ptr(void *ptr_addr);
+int		prt_int(int n);
+int		prt_unsigned(unsigned int nbr);
+int		prt_hexa(unsigned int nbr, bool upper_case);
+
+#endif
---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adamarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 11:12:30 by adamarqu          #+#    #+#             */
+/*   Updated: 2024/10/29 11:14:10 by adamarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+	int output_OG;
+	int output_FT;
+
+	// no specifiers
+	// %c
+	// %s
+	// %p
+	// %d
+	// %i
+	// &u
+	// &x
+	// %X
+	// & percentage tests
+	return (0);
+}
