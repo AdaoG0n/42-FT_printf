1af36ee88c0a5e478d5669d553b1caa93ed51de8
Adao Manuel Marques Goncalves <adamarqu@c1r10s5.42porto.com>
2024-11-04 14:32:03 +0000

    update functions

---
+++ ft_print_pointer.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_pointer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adamarqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 13:25:37 by adamarqu          #+#    #+#             */
+/*   Updated: 2024/11/04 13:28:31 by adamarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_printf.h"
+
+int	ft_print_pointer(void *ptr)
+{
+	int				count;
+	unsigned long	address;
+
+	count = 0;
+	address = (unsigned long)ptr;
+	count += write(1, "0x", 2);
+	count += ft_print_hex(address, 0);
+	return (count);
+}
